0346cb53841a8e3de3b205fd996ed98f05e58b88
g0kuu1 <gokule1100@gmail.com>
2026-09-20 14:05:43 +0530

    implement-posthog

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import posthog from "posthog-js";
 
 interface Props {
     title : string;
@@ -12,7 +15,18 @@ interface Props {
 
 const EventCard = ({title , image , slug , location, date , time}: Props) => {
   return (
-    <Link href={`/events/${slug}`} id="event-card">
+    <Link
+      href={`/events/${slug}`}
+      id="event-card"
+      onClick={() => {
+        if (
+          process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN &&
+          process.env.NEXT_PUBLIC_POSTHOG_HOST
+        ) {
+          posthog.capture("event_selected", { event_slug: slug });
+        }
+      }}
+    >
         <Image src={image} alt={title} height={300} width={410} className="poster"/>
 
         <div className="flex flex-row gap-2">
